36c54071db0d98d4a9f42998acacccca746d7a0f
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-04-27 19:17:45 -0300

    Create API

---
+++ index.js
@@ -0,0 +1,17 @@
+const app = require('express')()
+const PORT = 8080
+
+app.get('/multiply', (req, res) =>{
+    let num1 = parseInt(req.query.num1)
+    let num2 = parseInt(req.query.num2)
+
+    res.status(200).send({
+        'result': num1 * num2
+    })
+})
+
+app.listen(PORT, () =>{
+    console.log(`http://localhost:${PORT}`)
+})
+
+module.exports = app;
