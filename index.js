4d914ebc19fe2ea1a8a782ed4a73bbf1f320e2f7
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-04-27 19:55:32 -0300

    Update API's response from JSON to TEXT

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ app.get('/multiply', (req, res) =>{
     let num1 = parseInt(req.query.num1)
     let num2 = parseInt(req.query.num2)
 
-    res.status(200).send({
-        'result': num1 * num2
-    })
+    res.status(200).send(num1 * num2)
 })
 
 app.listen(PORT, () =>{
