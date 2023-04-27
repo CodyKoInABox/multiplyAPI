66f349475701426d619137748d0057dd89f422f6
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-04-27 20:13:28 -0300

    add support for addition subtraction and division to API

--- index.js
+++ index.js
@@ -24,6 +24,51 @@ app.get('/multiply', (req, res) =>{
     })
 })
 
+app.get('/add', (req, res) =>{
+    let num1 = parseInt(req.query.num1)
+    let num2 = parseInt(req.query.num2)
+
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+    });
+
+
+    res.status(200).send({
+        'result': (num1 + num2).toString()
+    })
+})
+
+app.get('/subtract', (req, res) =>{
+    let num1 = parseInt(req.query.num1)
+    let num2 = parseInt(req.query.num2)
+
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+    });
+
+
+    res.status(200).send({
+        'result': (num1 - num2).toString()
+    })
+})
+
+app.get('/divide', (req, res) =>{
+    let num1 = parseInt(req.query.num1)
+    let num2 = parseInt(req.query.num2)
+
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+    });
+
+
+    res.status(200).send({
+        'result': (num1 / num2).toString()
+    })
+})
+
 app.listen(PORT, () =>{
     console.log(`http://localhost:${PORT}`)
 })
