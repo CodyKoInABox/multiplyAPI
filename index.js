5e49a17932a53e294b9d5629cf9dcf84b68e5787
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-04-27 19:56:40 -0300

    fix api

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ app.get('/multiply', (req, res) =>{
     let num1 = parseInt(req.query.num1)
     let num2 = parseInt(req.query.num2)
 
-    res.status(200).send(num1 * num2)
+
+    res.status(200).send({
+        'result': num1 * num2
+    })
 })
 
 app.listen(PORT, () =>{
